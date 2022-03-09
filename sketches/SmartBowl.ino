873517399db1c02c7494333c60a0151ae9b4a349
Arif Bryan <arief372@gmail.com>
2022-03-09 15:28:31 +0700

    Add HX711 driver code

--- sketches/SmartBowl.ino
+++ sketches/SmartBowl.ino
@@ -1,7 +1,8 @@
 #include <BluetoothSerial.h>
 #include <Adafruit_GFX.h>    
 #include <Adafruit_ST7789.h> 
-#include <SPI.h>             
+#include <SPI.h>           
+#include <HX711.h>
 
 #define PWR_LATCH	2
 
@@ -10,9 +11,12 @@
 #define TFT_RST		22  
 #define TFT_DC		21  
 
+#define HX_DATA		27
+#define HX_SCK		26
+
 BluetoothSerial SerialBT;
 Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
-
+HX711 LoadCell;
 
 void setup(){
 	pinMode(PWR_LATCH, OUTPUT);
@@ -22,6 +26,7 @@ void setup(){
 	ledcWrite(0, 0);
 	delay(100);
 	
+	LoadCell.begin(HX_DATA, HX_SCK);
 	ledcWrite(0, 30);
 	tft.init(240, 240, SPI_MODE2);
 	tft.fillScreen(ST77XX_BLACK);
@@ -32,6 +37,7 @@ void setup(){
 uint32_t i;
 uint8_t val = 30;
 void loop(){
+	LoadCell.read();
 	ledcWrite(0, val);
 	SerialBT.println(i++);
 	tft.invertDisplay(true);
