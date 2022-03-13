3fa6f60a07af39c1795cea768f4c0888077db448
Arif Bryan <arief372@gmail.com>
2022-03-13 22:56:00 +0700

    Functionality testing

--- sketches/SmartBowl.ino
+++ sketches/SmartBowl.ino
@@ -5,6 +5,10 @@
 #include <HX711.h>
 
 #define PWR_LATCH	2
+#define BTN_PWR		17
+#define CHG_CON		4
+#define CHG_STA		16
+#define VBAT_SENSE	33
 
 #define TFT_BKLT	19
 #define TFT_CS		35
@@ -20,6 +24,9 @@ HX711 LoadCell;
 
 void setup(){
 	pinMode(PWR_LATCH, OUTPUT);
+	pinMode(CHG_CON, INPUT_PULLUP);
+	pinMode(CHG_STA, INPUT_PULLUP);
+	pinMode(BTN_PWR, INPUT_PULLUP);
 	digitalWrite(PWR_LATCH, HIGH);
 	ledcSetup(0, 5000, 8);
 	ledcAttachPin(TFT_BKLT, 0);
@@ -29,19 +36,72 @@ void setup(){
 	LoadCell.begin(HX_DATA, HX_SCK);
 	ledcWrite(0, 30);
 	tft.init(240, 240, SPI_MODE2);
-	tft.fillScreen(ST77XX_BLACK);
+	tft.setRotation(1);
+	tft.fillScreen(ST77XX_WHITE);
   
-	SerialBT.begin("ESP32");
+	Serial.begin(115200);
+	//SerialBT.begin("ESP32");
 }
 
 uint32_t i;
 uint8_t val = 30;
+float adcFiltered;
+uint8_t kf = 3;
+uint32_t vbat;
+uint32_t dspTmr;
 void loop(){
-	LoadCell.read();
-	ledcWrite(0, val);
-	SerialBT.println(i++);
-	tft.invertDisplay(true);
-	delay(500);
-	tft.invertDisplay(false);
-	delay(500);
+	adcFiltered = ((adcFiltered * kf) + LoadCell.get_value(1)) / (kf + 1);
+	vbat = analogRead(VBAT_SENSE);
+	Serial.println(vbat);
+	if (millis() - dspTmr >= 250) {
+		dspTmr = millis();
+		if (!digitalRead(CHG_CON)) {
+			if (!digitalRead(CHG_STA)) {
+				tft.fillRect(0, 0, 240, 24, ST77XX_ORANGE);
+			}
+			else {
+				tft.fillRect(0, 0, 240, 24, ST77XX_GREEN);
+			}
+		}
+		else if (!digitalRead(BTN_PWR)) {
+			tft.fillRect(0, 0, 240, 24, ST77XX_BLUE);
+			LoadCell.tare();
+		}
+		else {	
+			tft.fillRect(0, 0, 240, 24, 0x1111);
+		}
+		tft.setTextColor(ST77XX_WHITE);
+		tft.setCursor(1, 1);
+		tft.setTextSize(3);
+		tft.print("Test");
+		
+		tft.setTextColor(ST77XX_BLACK, ST77XX_WHITE);
+		tft.setCursor(1, 26);
+		tft.print("Val. : ");
+		tft.print(adcFiltered);
+		tft.print(" ");
+		
+		tft.setCursor(1, 52);
+		tft.print("VBat : ");
+		tft.print(vbat);
+		tft.print(" ");
+		
+		tft.setCursor(1, 78);
+		if (!digitalRead(CHG_CON)) {
+			if (!digitalRead(CHG_STA)) {
+				tft.print("Charging     ");
+			}
+			else {
+				tft.print("Charged      ");
+			}
+		}
+		else {
+			tft.print("Not charging ");
+		}
+	}
+//	ledcWrite(0, val);
+//	SerialBT.println(i++);
+//	tft.invertDisplay(true);
+//	delay(100);
+//	tft.invertDisplay(false);
 }
