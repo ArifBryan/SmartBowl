99db18a5660a688b6ef6e4e198afccb556726ce6
Arif Bryan <arief372@gmail.com>
2022-09-04 15:19:55 +0700

    Add display test.

--- sketches/UserInterface.h
+++ sketches/UserInterface.h
@@ -6,6 +6,9 @@ struct UserInterface_TypeDef {
 	void Init();
 	void Handler();
 	void SetBrightness(uint8_t bright);
+	
+private:
+	uint32_t dispTimer;
 };
 
 extern UserInterface_TypeDef ui;
--- sketches/UserInterface.ino
+++ sketches/UserInterface.ino
@@ -2,6 +2,7 @@
 #include "Pins.h"
 #include <Adafruit_GFX.h>    
 #include <Adafruit_ST7789.h> 
+#include <Fonts/FreeSans12pt7b.h>
 
 UserInterface_TypeDef ui;
 
@@ -16,6 +17,8 @@ void UserInterface_TypeDef::Init() {
 	ledcSetup(0, 5000, 8);
 	ledcAttachPin(TFT_BKLT, 0);
 	
+	lcd.setFont(&FreeSans12pt7b);
+	lcd.setTextColor(ST77XX_BLACK);
 	lcd.init(240, 240, SPI_MODE2);
 	lcd.setRotation(1);
 	lcd.fillScreen(ST77XX_WHITE);
@@ -23,5 +26,17 @@ void UserInterface_TypeDef::Init() {
 }
 
 void UserInterface_TypeDef::Handler() {
-	
+	if (millis() - dispTimer >= 250) {
+		GFXcanvas16 frame1(240, 20);
+		
+		frame1.setFont(&FreeSans12pt7b);
+		frame1.setTextColor(ST77XX_BLACK);
+		frame1.fillScreen(ST77XX_WHITE);
+		frame1.setCursor(0, 16);
+		frame1.print("Test");
+		
+		lcd.drawRGBBitmap(0, 0, frame1.getBuffer(), 240, 20);
+		
+		dispTimer = millis();
+	}
 }
