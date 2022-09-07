e0d765005e4a0fbad7e651080ed7f69c15f87180
Arif Bryan <arief372@gmail.com>
2022-09-07 18:09:09 +0700

    Move vars

--- sketches/Sensor.h
+++ sketches/Sensor.h
@@ -5,6 +5,10 @@
 struct Sensor_TypeDef{
 	void Init();
 	void Handler();
+	
+private:
+	uint32_t sampleTmr;
+
 };
 
 extern Sensor_TypeDef sens;
--- sketches/Sensor.ino
+++ sketches/Sensor.ino
@@ -6,15 +6,12 @@ Sensor_TypeDef sens;
 
 HX711 LoadCell;
 
-uint32_t loadCellSampleInterval;
-uint32_t loadCellSampleTmr;
-
 void Sensor_TypeDef::Init() {
 	LoadCell.begin(HX_DATA, HX_SCK);
 }
 
 void Sensor_TypeDef::Handler() {
-	if (millis() - loadCellSampleTmr >= loadCellSampleInterval) {
-		loadCellSampleTmr = millis();
+	if (millis() - sampleTmr >= 250) {
+		sampleTmr = millis();
 	}
 }
